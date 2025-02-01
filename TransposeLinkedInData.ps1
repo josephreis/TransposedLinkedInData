a175b315f587e5d7c41f844b8f3540acfba4b0f2
josephreis <reis.joseph.m@gmail.com>
2025-02-01 00:02:43 -0800

    Update

---
+++ TransposeLinkedInData.ps1
@@ -0,0 +1,736 @@
+$data = @"
+MagniFind
+Vice President of Engineering
+MagniFind
+California, United States (Remote)
+Applied 4h ago
+
+Pyramid Consulting, Inc
+ IT Systems Engineer
+Pyramid Consulting, Inc
+United States (Remote)
+Applied 18h ago
+
+Denu Recruit
+Platform Engineer - AWS, Kubernetes, CDK
+Denu Recruit
+United States (Remote)
+Applied 18h ago
+
+Bison Global Technology Search
+Site Reliability Engineer
+Bison Global Technology Search
+United States (Remote)
+Applied 18h ago
+
+AddSource 
+Cloud Infrastructure Engineer
+AddSource 
+United States (Remote)
+Applied 18h ago
+
+Contrast Security
+Senior Cloud Engineer
+Contrast Security
+United States (Remote)
+Applied 18h ago
+
+Pyramid Consulting, Inc
+IT Systems Engineer Lead
+Pyramid Consulting, Inc
+United States (Remote)
+Applied 19h ago
+
+Upstart
+Principal IT Engineer , Verified
+Upstart
+United States (Remote)
+Applied 19h ago
+
+Gemini
+Senior Enterprise Infrastructure Engineer
+Gemini
+United States (Remote)
+Applied 19h ago
+
+Centific
+Senior Solutions Engineer , Verified
+Centific
+San Francisco Bay Area (Remote)
+Applied 19h ago
+
+JustinBradley
+AWS DevOps Engineer
+JustinBradley
+United States (Remote)
+Applied 19h ago
+
+COCKROACH LABS, INC.
+Senior Technical Support Engineer - West/Central US , Verified
+Cockroach Labs
+New York, NY (Remote)
+Applied 19h ago
+
+Jobot
+Infrastructure/DevOps Engineer , Verified
+Jobot
+Bakersfield, CA (Remote)
+Applied 19h ago
+
+Palo Alto Networks
+Principal Engineer, Engineering Productivity , Verified
+Palo Alto Networks
+Santa Clara, CA (Remote)
+Applied 20h ago
+
+GAC Solutions
+AWS Technical Manager 
+GAC Solutions
+United States (Remote)
+Applied 20h ago
+
+Jobot
+Infrastructure/DevOps Engineer , Verified
+Jobot
+Redding, CA (Remote)
+Applied 20h ago
+
+INSPYR Solutions
+Infrastructure Architect - Direct-Hire/FTE - Los Angeles or Washington DC. , Verified
+INSPYR Solutions
+Los Angeles, CA (Remote)
+Resume downloaded 20h ago
+
+BlackLine Systems
+Staff I Reliability Engineer , Verified
+BlackLine
+Los Angeles, CA (Hybrid)
+Applied 20h ago
+
+Hyundai Autoever America
+10575 - Sr. Application/Solution Architect , Verified
+Hyundai AutoEver America
+Costa Mesa, CA (Hybrid)
+Applied 20h ago
+
+Apex Informatics
+ServiceNow Administrator
+Apex Informatics
+Atlanta, GA (Remote)
+Applied 20h ago
+
+PriceSenz
+Organizational Change Manager
+PriceSenz
+Texas, United States (Remote)
+Applied 20h ago
+
+cFocus Software Incorporated
+Configuration Manager
+cFocus Software Incorporated
+United States (Remote)
+Applied 20h ago
+
+Connect Tech+Talent
+Organizational Change Management (5068525OCM2)
+Connect Tech+Talent
+United States (Remote)
+Applied 20h ago
+
+Anderson Global
+Technical Application Manager
+Anderson Global
+United States (Remote)
+Applied 20h ago
+
+GlobalLogic
+Site Reliability and Operations Lead , Verified
+GlobalLogic
+United States (Remote)
+Applied 20h ago
+
+BETSOL
+Technical Lead , Verified
+BETSOL
+United States (Remote)
+Applied 20h ago
+
+Roku
+Partner Engagement Manager , Verified
+Roku
+Austin, TX (On-site)
+Applied 1d ago
+
+Roku
+Director, Cloud Security , Verified
+Roku
+Austin, TX (On-site)
+Applied 1d ago
+
+Roku
+Senior Director of Cloud Platform Engineering & Infrastructure , Verified
+Roku
+Cardiff (On-site)
+Applied 1d ago
+
+Nextiva
+Platform Engineering Manager , Verified
+Nextiva
+Scottsdale, AZ (Remote)
+Applied 1d ago
+
+Included Health
+Engineering Manager, Engage , Verified
+Included Health
+United States (Remote)
+Applied 1d ago
+
+Recruiting from Scratch
+Head of Engineering
+Recruiting from Scratch
+San Francisco Bay Area (Remote)
+Applied 2d ago
+
+Instabase
+Engineering Manager, Infrastructure , Verified
+Instabase
+San Francisco, CA (Hybrid)
+Applied 2d ago
+
+Atlan
+Sales Engineering Manager , Verified
+Atlan
+Texas, United States (Remote)
+Applied 2d ago
+
+Headspace Meditation Limited
+Engineering Manager , Verified
+Headspace
+United States (Remote)
+Applied 2d ago
+
+SoftServe
+AWS Cloud Architect , Verified
+SoftServe
+United States (Remote)
+Applied 2d ago
+
+Dragonfly
+Technical Solutions Engineer
+Dragonfly
+United States (Remote)
+Applied 2d ago
+
+Blockchain Staffing Ninja
+Distributed Systems Engineer
+Blockchain Staffing Ninja
+United States (Remote)
+Applied 2d ago
+
+Airitos, LLC
+Senior Solutions Architect
+Airitos, LLC
+United States (Remote)
+Applied 2d ago
+
+Sakin Technologies LLC.
+Aws Cloud Architect
+Sakin Technologies LLC.
+United States (Remote)
+Resume downloaded 2d ago
+
+Christy Media Solutions
+Support Engineer , Verified
+Christy Media Solutions
+United States (Remote)
+Applied 2d ago
+
+SGNL
+Senior Cloud Infrastructure Engineer
+SGNL
+Palo Alto, CA (Remote)
+Applied 2d ago
+
+Motion Recruitment
+Security Engineer - w2 only - no H1/C2C , Verified
+Motion Recruitment
+United States (Remote)
+Applied 2d ago
+
+AddSource 
+OpenStack Engineer
+AddSource 
+United States (Remote)
+Applied 2d ago
+
+Tek Tron IT
+DevOps Engineer
+Tek Tron IT
+United States (Remote)
+Applied 2d ago
+
+ESB Technologies
+Information Technology Solutions Architect
+ESB Technologies
+United States (Remote)
+Applied 2d ago
+
+Recruiting from Scratch
+Senior Cloud Engineer
+Recruiting from Scratch
+United States (Remote)
+Applied 2d ago
+
+Professional Employment Group of Colorado
+Sr. Site Reliability Engineer 
+Professional Employment Group of Colorado
+United States (Remote)
+Applied 2d ago
+
+Amtex Systems Inc.
+Cloud Architect , Verified
+Amtex Systems Inc.
+United States (Remote)
+Applied 2d ago
+
+DevOps Engineer
+technology
+DevOps Engineer
+United States (Remote)
+Applied 2d ago
+
+Bearing
+Solutions Engineer
+Bearing
+United States (Remote)
+Applied 2d ago
+
+Tredence Inc.
+AWS Cloud Infrastructure Architect , Verified
+Tredence Inc.
+United States (Remote)
+Applied 2d ago
+
+IT Minds LLC
+HPC System Engineer @remote
+IT Minds LLC
+United States (Remote)
+Applied 2d ago
+
+Nelson Connects
+Security Engineer , Verified
+Nelson Connects
+Mountain View, CA (Hybrid)
+Applied 2d ago
+
+Facebook
+Hardware System Engineer
+Facebook
+Sunnyvale, CA (On-site)
+Applied 2d ago
+
+Digital Prospectors
+Senior IT DevOps Engineer , Verified
+Digital Prospectors
+Palo Alto, CA (Hybrid)
+Applied 2d ago
+
+LeagueApps
+Engineering Manager (Schedules) , Verified
+LeagueApps
+New York, NY
+Applied 2d ago
+
+Amtex Systems Inc.
+Cloud Engineer , Verified
+Amtex Systems Inc.
+United States (Remote)
+Applied 2d ago
+
+Madison-Davis, LLC
+DevOps SRE , Verified
+Madison-Davis, LLC
+United States (Remote)
+Application viewed 2d ago
+
+Verigent
+Data Center Engineer , Verified
+Verigent
+Texas, United States (Remote)
+Applied 2d ago
+
+HKA Enterprises
+Cloud Migration Architect , Verified
+HKA Enterprises
+United States (Remote)
+Applied 2d ago
+
+Akkodis
+Cloud Architect , Verified
+Akkodis
+Greater Minneapolis-St. Paul Area (Remote)
+Applied 2d ago
+
+CyberSheath
+Cloud Operations Engineer
+CyberSheath
+Reston, VA (Remote)
+Applied 2d ago
+
+Eviden
+Azure Architect
+Eviden
+Buffalo, NY (Remote)
+Applied 2d ago
+
+Milestone Technologies, Inc.
+Release Train Engineer , Verified
+Milestone Technologies, Inc.
+California, United States (Remote)
+Applied 2d ago
+
+Harvey Nash
+Virtualization Engineer
+Harvey Nash
+California, United States (Remote)
+Applied 2d ago
+
+VanderHouwen
+Director of Engineering , Verified
+VanderHouwen
+United States (Remote)
+Applied 2d ago
+
+O'Reilly Auto Parts
+Technical Engineering Manager , Verified
+O'Reilly Auto Parts
+Missouri, United States (Remote)
+Applied 2d ago
+
+Mavely
+Staff Engineer
+Mavely
+United States (Remote)
+Applied 2d ago
+
+Timo Talent
+Director of Engineering
+Timo Talent
+Nashville, TN (Remote)
+Applied 2d ago
+
+Irvine Technology Corporation
+Senior System Engineer
+Irvine Technology Corporation
+Charlotte Metro (Remote)
+Applied 2d ago
+
+Hire IT People, Inc
+Cloud Engineer (Azure or GCP) - Full time (remote) , Verified
+Hire IT People, Inc
+New York City Metropolitan Area (Remote)
+Applied 2d ago
+
+Medix Technology
+Senior Systems Engineer - VMware , Verified
+Medix Technology
+United States (Remote)
+Applied 2d ago
+
+Talent Groups
+AWS Cloud Architect
+Talent Groups
+United States (Remote)
+Applied 2d ago
+
+Dexian
+ Dynatrace Consultant , Verified
+Dexian
+Eden Prairie, MN (Remote)
+Application viewed 2d ago
+
+Walter Bacon, Inc.
+Solutions Engineer (K8s Startup)
+Walter Bacon, Inc.
+Texas, United States (Remote)
+Applied 2d ago
+
+Gemini
+Senior Site Reliability Engineer, Onchain
+Gemini
+United States (Remote)
+Applied 2d ago
+
+Driivz
+Site Reliability Engineer
+Driivz
+Houston, TX (Remote)
+Applied 2d ago
+
+Zeektek
+Site Reliability Engineer (Dynatrace)
+Zeektek
+United States (Remote)
+Application viewed 2d ago
+
+Zeektek
+Dynatrace Engineer - 2-4 years of experience (W2 Contract-to-hire, no C2C)
+Zeektek
+United States (Remote)
+Application viewed 2d ago
+
+HUB International
+Manager, Backup and Disaster Recovery , Verified
+HUB International
+United States (Remote)
+Applied 2d ago
+
+REQ Solutions
+Technical Team Lead
+REQ Solutions
+San Francisco, CA (Remote)
+Applied 2d ago
+
+Interactive Resources - iR
+IT Manager , Verified
+Interactive Resources - iR
+United States (Remote)
+Applied 2d ago
+
+LaSalle Network
+Remote Mainframe Transition Manager , Verified
+LaSalle Network
+Chicago, IL (Remote)
+Applied 2d ago
+
+IT Associates
+Mainframe Transition Manager 
+IT Associates
+United States (Remote)
+Applied 2d ago
+
+Rackspace Technology
+Delivery Leader Cloud Managed Services/Senior Manager Cloud Practice , Verified
+Rackspace Technology
+United States (Remote)
+Applied 2d ago
+
+CAVA
+Sr. Manager IT Service Management & Operations , Verified
+CAVA
+Washington DC-Baltimore Area (Remote)
+Applied 2d ago
+
+EVONA
+Senior Manager, Data Architecture & Networking Solutions
+EVONA
+United States (Remote)
+Applied 2d ago
+
+Dexian
+Data Center Integration & Technical Infrastructure Manager , Verified
+Dexian
+United States (Remote)
+Applied 2d ago
+
+IT Concepts, Inc
+Sr. Integration Engineer , Verified
+IT Concepts, Inc
+United States (Remote)
+Applied 2d ago
+
+Wave Talent
+Artificial Intelligence Engineer
+Wave Talent
+United States (Remote)
+Applied 2d ago
+
+Protingent
+IT Configuration Management Specialist
+Protingent
+Bellevue, WA
+Applied 2d ago
+
+Orbis Group
+Staff Engineer – Distributed Systems – Staff Engineer – New York City / San Francisco / Seattle (Remote from these locations) – Competitive Salary + Equity , Verified
+Orbis Group
+New York City Metropolitan Area (Remote)
+Applied 2d ago
+
+HireVue
+Cloud Architect | Fully Remote US , Verified
+HireVue
+South Jordan, UT (Remote)
+Applied 2d ago
+
+Raydar
+Forward Deployed Engineer
+Raydar
+United States (Remote)
+Applied 2d ago
+
+Digineox
+Cloud CI/CD Architect - DevOps & Cloud Infrastructure
+Digineox
+United States (Remote)
+Applied 2d ago
+
+Integrated Management Resources, LLC.
+Production Support Engineer
+Integrated Management Resources, LLC.
+Dallas, TX (Remote)
+Applied 2d ago
+
+Compunnel Inc.
+DevOps Engineer
+Compunnel Inc.
+Irvine, CA (Hybrid)
+Applied 2d ago
+
+Centric Software
+Cloud Operations Engineer , Verified
+Centric Software
+United States (Remote)
+Applied 2d ago
+
+Revel IT
+Gitlab Devops Consultant
+Revel IT
+Columbus, Ohio Metropolitan Area (Remote)
+Applied 2d ago
+
+LHH
+DevOps Engineer , Verified
+LHH
+United States (Remote)
+Applied 2d ago
+
+Motion Recruitment
+System Engineer , Verified
+Motion Recruitment
+California, United States (Remote)
+Applied 2d ago
+
+SoFi
+Senior Workday Financials Integration Engineer , Verified
+SoFi
+Williamson County, TX (Remote)
+Applied 2d ago
+
+Headspace Meditation Limited
+Engineering Manager, Engagement , Verified
+Headspace
+United States (Remote)
+Applied 2d ago
+
+Harnham
+Platform Engineer , Verified
+Harnham
+United States (Remote)
+Applied 2d ago
+
+Riot Games - US
+Systems Engineer II - Infrastructure , Verified
+Riot Games
+Los Angeles, CA
+Applied 2d ago
+
+PuppySpot
+Cloud Data Engineer
+PuppySpot
+United States (Remote)
+Applied 2d ago
+
+Hammerspace, Inc.
+Staff Software Platform-DevOps Engineer , Verified
+Hammerspace
+United States (Remote)
+Applied 2d ago
+
+Hyundai Autoever America
+10773 - Sr. System Engineer , Verified
+Hyundai AutoEver America
+Costa Mesa, CA (Hybrid)
+Applied 2d ago
+
+Jobot
+Infrastructure/DevOps Engineer , Verified
+Jobot
+Oakland, CA (Remote)
+Applied 2d ago
+
+NBCUniversal
+Manager, Playout Engineering , Verified
+NBCUniversal
+Englewood Cliffs, NJ (Remote)
+Applied 2d ago
+
+NBCUniversal
+Staff Site Reliability Engineer , Verified
+NBCUniversal
+Universal City, CA (Remote)
+Applied 3d ago
+
+COCKROACH LABS, INC.
+Incident Manager - West/Central US , Verified
+Cockroach Labs
+New York, NY (Remote)
+Applied 3d ago
+
+University of California, San Francisco
+Monitoring Engineer
+University of California, San Francisco
+San Francisco, CA (On-site)
+Applied 3d ago
+
+Chipotle Mexican Grill
+Lead Data Engineer , Verified
+Chipotle Mexican Grill
+Columbus, OH (On-site)
+Application viewed 1w ago
+
+TikTok
+Major Incident Manager, Eng Support-Incident Management Team - USDS , Verified
+TikTok
+Seattle, WA (Hybrid)
+Applied 1w ago
+
+Primis 
+Infrastructure Engineer
+Primis 
+United States (Remote)
+Applied 1w ago
+
+TikTok
+Major Incident Manager, Eng Support-Incident Management Team - USDS , Verified
+TikTok
+Los Angeles, CA (Hybrid)
+Applied 1w ago
+
+Optomi
+Monitoring Engineer (Elastic) , Verified
+Optomi
+United States (Remote)
+Applied 1w ago
+
+Centific
+Operations Lead - Solutions Team , Verified
+Centific
+Washington, United States (Remote)
+Applied 2mo ago
+"@ -split "`r`n"
+
+
+$transposedData = @()
+$transposedData += for ($i = 0; $i -lt $data.Length; $i += 6){
+
+                    [PSCustomObject]@{
+                        'Company Name' = ($data[0+$i]).trim()
+                        'Position Title' = ($data[1+$i]).trim()
+                        'Location' = ($data[3+$i]).trim()
+                        'Last Time Applied or Viewed' = ($data[4+$i]).trim()
+                        }
+                    }
+
+$transposedData | Out-GridView
+
